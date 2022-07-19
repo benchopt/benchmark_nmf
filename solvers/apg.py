4e6b03f8a3c673dca43096447e7121f069b3f46c
COHEN Jeremy <jeremy.cohen@cnrs.fr>
2022-07-19 14:51:00 +0200

    debugging apg

--- solvers/apg.py
+++ solvers/apg.py
@@ -15,37 +15,36 @@ class Solver(BaseSolver):
         'n_inner_iter': [1, 5]
     }
 
-    def set_objective(self, X):
+    def set_objective(self, X, rank):
         # The arguments of this function are the results of the
         # `to_dict` method of the objective.
         # They are customizable.
         self.X = X
+        self.rank = rank
 
-    def run(self, n_iter, n_inner_iter):
+    def run(self, n_iter):
         m, n = self.X.shape
         rank = self.rank
+        n_inner_iter = self.n_inner_iter
 
         # initialization with random values (TODO: improve?)
-        W = np.zeros((m,rank))
-        H = np.zeros((rank,n))
+        self.fac = [np.random.rand(m,rank),np.random.rand(rank,n)]
 
         for _ in range(n_iter):
-            HHt = np.dot(H, H.T)
-            XHt = np.dot(self.X, H.T)
-            Lw = np.linalg.norm(HHt,2)
+            HHt = np.dot(self.fac[1], self.fac[1].T)
+            XHt = np.dot(self.X, self.fac[1].T)
+            Lw = np.linalg.norm(HHt)  # upper bound of Lw
             # W update
             for inner in range(n_inner_iter):
-                W -= (np.dot(W, HHt) - XHt ) / Lw
+                self.fac[0] = np.maximum(self.fac[0] - (np.dot(self.fac[0], HHt) - XHt ) / Lw, 0)
 
             # H update
-            WtW = np.dot(W.T, W)
-            WtX = np.dot(W.T, self.X)
-            Lh = np.linalg.norm(WtW,2)
-            # W update
+            WtW = np.dot(self.fac[0].T, self.fac[0])
+            WtX = np.dot(self.fac[0].T, self.X)
+            Lh = np.linalg.norm(WtW)  # upper bound for Lh
+            # H update
             for inner in range(n_inner_iter):
-                H -= (np.dot(WtW, H) - WtX ) / Lh
-
-        self.fac = [W, H]
+                self.fac[1] = np.maximum(self.fac[1] - (np.dot(WtW, self.fac[1]) - WtX ) / Lh, 0)
 
     def get_result(self):
         # The outputs of this function are the arguments of the
