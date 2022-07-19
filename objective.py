284da192afd84d15d91ec39a80bf11ead61274db
COHEN Jeremy <jeremy.cohen@cnrs.fr>
2022-07-19 10:34:08 +0200

    first version of objective

--- objective.py
+++ objective.py
@@ -7,32 +7,37 @@ with safe_import_context() as import_ctx:
 
 
 class Objective(BaseObjective):
-    name = "Ordinary Least Squares"
+    name = "Nonnegative Matrix Factorization"
 
     # All parameters 'p' defined here are available as 'self.p'
     parameters = {
         'fit_intercept': [False],
+        'rank': 1 #TODO check right pos?
     }
 
     def get_one_solution(self):
         # Return one solution. This should be compatible with 'self.compute'.
-        return np.zeros(self.X.shape[1])
+        # Solutions of NMF are length two lists with W and H
+        m, n = self.X.shape
+        rank = self.rank
+        return [np.zeros((m, rank)),np.zeros((rank, n))]
 
-    def set_data(self, X, y):
+    def set_data(self, X, rank):
         # The keyword arguments of this function are the keys of the `data`
         # dict in the `get_data` function of the dataset.
         # They are customizable.
-        self.X, self.y = X, y
+        self.X, self.rank = X, rank
 
-    def compute(self, beta):
+    def compute(self):
         # The arguments of this function are the outputs of the
         # `get_result` method of the solver.
         # They are customizable.
-        diff = self.y - self.X.dot(beta)
-        return .5 * diff.dot(diff)
+        # TODO: also allow other losses
+        frob = np.linalg.norm(self.X - self.fac[0]@self.fac[1])**2 # TODO: self.fac
+        return frob
 
     def to_dict(self):
         # The output of this function are the keyword arguments
         # for the `set_objective` method of the solver.
         # They are customizable.
-        return dict(X=self.X, y=self.y, fit_intercept=self.fit_intercept)
+        return dict(X=self.X)
